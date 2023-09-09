a968d5284adc8a201c5b170ba48a7dabe453c77c
eoryazantseva <eoryazantseva@gmail.com>
2023-09-09 12:42:41 +1000

    the post fetch for the login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,46 @@
+import { useState } from "react";
+
+import postLogin from "../api/post-login.js";
+
 function LoginForm() {
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: "",
+    });
+
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        setCredentials((prevCredentials) => ({
+            ...prevCredentials,
+            [id]: value,
+        }));
+    };
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (credentials.username && credentials.password) {
+            postLogin(
+                credentials.username,
+                credentials.password
+                ).then((response) => {
+                    console.log(response);
+                });
+        }
+    };
+
     return (
         <form>
             <div>
                 <label htmlFor="username">Username:</label>
-                <input type="text" id="username" placeholder="Enter username" />
+                <input type="text" id="username" placeholder="Enter username" onChange={handleChange} />
             </div>
             
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" placeholder="Password" />
+                <input type="password" id="password" placeholder="Password" onChange={handleChange}/>
             </div>
-            <button type="submit">Login</button>
+            <button type="submit" onClick={handleSubmit}>Login</button>
         </form>
     );
 }
